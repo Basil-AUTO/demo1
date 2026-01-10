a823877a0078bc4c25101edaff153a2142fed4c5
Basil S. Elbalaawi <basil@ps-ix.ps>
2026-01-10 20:14:01 +0200

    Update Person.java

--- src/main/java/com/example/model/Person.java
+++ src/main/java/com/example/model/Person.java
@@ -3,7 +3,7 @@ package com.example.model;
 public class Person {
 	//Test comment added
 	// add comment 2
-	// test adding email notifdidaghtion
+	// test adding email notifdidahtion
 	String id;
 	String firstName;
 	String lastName;
