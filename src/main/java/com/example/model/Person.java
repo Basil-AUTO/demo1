1da89a96c34866e40ac54fbc47bc1c58796345d3
Basil S. Elbalaawi <basil@ps-ix.ps>
2026-01-10 19:50:07 +0200

    Fix typo in email notification comment

--- src/main/java/com/example/model/Person.java
+++ src/main/java/com/example/model/Person.java
@@ -3,7 +3,7 @@ package com.example.model;
 public class Person {
 	//Test comment added
 	// add comment 2
-	// test adding email notifdicfdation
+	// test adding email notifdifdation
 	String id;
 	String firstName;
 	String lastName;
