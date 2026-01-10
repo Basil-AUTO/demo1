4d141009045c41cec069a4a397feb3e52f2f3e26
Basil S. Elbalaawi <basil@ps-ix.ps>
2026-01-10 20:23:20 +0200

    Fix comment typo in Person.java

--- src/main/java/com/example/model/Person.java
+++ src/main/java/com/example/model/Person.java
@@ -3,7 +3,7 @@ package com.example.model;
 public class Person {
 	//Test comment added
 	// add comment 2
-	// test adding email notifdidahtion
+	// test adding email notifidahtion
 	String id;
 	String firstName;
 	String lastName;
